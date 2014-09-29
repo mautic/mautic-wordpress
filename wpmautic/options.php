01f7676179d1cf4fbb0c21c4b08f126ace53887a
David Hurley <david@websparkinc.com>
2014-09-29 18:01:44 -0400

    Initial commit for WordPress plugin

---
+++ wpmautic/options.php
@@ -0,0 +1,42 @@
+<?php
+
+function wpmautic_options_page() 
+{ ?>
+	 <div>
+		<h2>WP Mautic</h2>
+		Enable Base URL for Mautic Integration.
+		<form action="options.php" method="post">
+			<?php settings_fields('wpmautic_options'); ?>
+			<?php do_settings_sections('wpmautic'); ?>
+			<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+		</form>
+	</div>
+<?php
+}
+
+add_action('admin_init', 'wpmautic_admin_init');
+
+function wpmautic_admin_init()
+{
+	register_setting( 'wpmautic_options', 'wpmautic_options', 'wpmautic_options_validate' );
+	add_settings_section('wpmautic_main', 'Main Settings', 'wpmautic_section_text', 'wpmautic');
+	add_settings_field('wpmautic_base_url', 'Base URL', 'wpmautic_base_url', 'wpmautic', 'wpmautic_main');
+}
+
+function wpmautic_section_text() 
+{
+}
+
+function wpmautic_base_url() 
+{
+	$options = get_option('wpmautic_options');
+	echo "<input id='wpmautic_base_url' name='wpmautic_options[base_url]' size='40' type='text' value='{$options['base_url']}' />";
+}
+
+function wpmautic_options_validate($input) 
+{
+	$options = get_option('wpmautic_options');
+	$options['base_url'] = trim($input['base_url']);
+	
+	return $options;
+}
