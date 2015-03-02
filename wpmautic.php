3202f4df341d807dfef3b8e1a5dcc9e6f95bab95
Jan Linhart <admin@escope.cz>
2015-03-02 11:30:43 +0100

    Make the tracking image invisible

--- wpmautic.php
+++ wpmautic.php
@@ -53,7 +53,7 @@ function wpmautic_function( $atts, $content = null )
 	$url_query = wpmautic_get_url_query();
 	$encoded_query = urlencode(base64_encode(serialize($url_query)));
 
-	$image   = '<img src="' . trim($options['base_url'], ' \t\n\r\0\x0B/') . '/mtracking.gif?d=' . $encoded_query . '" />';
+	$image   = '<img style="display:none" src="' . trim($options['base_url'], ' \t\n\r\0\x0B/') . '/mtracking.gif?d=' . $encoded_query . '" />';
 
 	echo $image;
 }
