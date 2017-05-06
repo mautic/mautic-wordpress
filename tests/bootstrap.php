34a86690487030fc4259b77a6eebc665c9b81d85
Stéphane HULARD <s.hulard@chstudio.fr>
2017-05-06 18:33:29 +0200

    Add TravisCI configuration.
    Also remove tests folder from Code Sniffer.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,13 +9,13 @@ echo 'Welcome to the WordPress Mautic Test Suite' . PHP_EOL . PHP_EOL;
 
 // Load plugin within wordpress.
 $GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( 'mautic-wordpress/wpmautic.php' ),
+	'active_plugins' => array( 'mautic-wordpress/wpmautic.php' ),
 );
 
 // Allow to define where WordPress source is installed.
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-    require rtrim(getenv( 'WP_DEVELOP_DIR' ), DIRECTORY_SEPARATOR) . '/tests/phpunit/includes/bootstrap.php';
-// Or assert that we are inside WordPress plugins directory.
+	require rtrim( getenv( 'WP_DEVELOP_DIR' ), DIRECTORY_SEPARATOR ) . '/tests/phpunit/includes/bootstrap.php';
+	// Or assert that we are inside WordPress plugins directory.
 } else {
-    require __DIR__.'/../../../../tests/phpunit/includes/bootstrap.php';
+	require __DIR__ . '/../../../../tests/phpunit/includes/bootstrap.php';
 }
