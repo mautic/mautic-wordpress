88ffb9e93372de47ae23c9c690578612d770e549
Stéphane HULARD <s.hulard@chstudio.fr>
2017-05-25 09:54:03 +0200

    Fix error when the script tag is displayed escaped.

--- wpmautic.php
+++ wpmautic.php
@@ -66,18 +66,16 @@ function wpmautic_function() {
 		return;
 	}
 
-	$javascript = <<<HTML
-<script>
-    (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
-        w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
-        m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','{$base_url}/mtc.js','mt');
+	$javascript = <<<JAVASCRIPT
+(function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+    w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+    m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+})(window,document,'script','{$base_url}/mtc.js','mt');
 
-    mt('send', 'pageview');
-</script>
-HTML;
+mt('send', 'pageview');
+JAVASCRIPT;
 
-	echo esc_js( $javascript );
+	?><script type="text/javascript"><?php echo esc_js( $javascript ); ?></script><?php
 }
 
 /**
