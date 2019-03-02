5a11426948fa60c74da570e0808bbaad46da57f5
Stéphane Hulard <s.hulard@chstudio.fr>
2019-03-02 12:15:34 +0100

    Add clean message about WP_DEVELOP_DIR variable when running tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,21 @@ $GLOBALS['wp_tests_options'] = array(
 
 // Allow to define where WordPress source is installed.
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-    require rtrim( getenv( 'WP_DEVELOP_DIR' ), DIRECTORY_SEPARATOR ) . '/tests/phpunit/includes/bootstrap.php';
+    $path = rtrim( getenv( 'WP_DEVELOP_DIR' ), DIRECTORY_SEPARATOR ) . '/tests/phpunit/includes/bootstrap.php';
+    if (!is_readable($path)) {
+        $message = <<<ERROR
+To run tests, you must define the 'WP_DEVELOP_DIR' with the path to your WordPress Development version checkout.
+For example :
+    export WP_DEVELOP_DIR="/home/development/wordpress-develop";
+
+You can also specify the variable on the fly when running tests :
+    WP_DEVELOP_DIR=/home/development/wordpress-develop vendor/bin/phpunit
+ERROR;
+        echo $message;
+        exit(1);
+    }
+
+    require $path;
     // Or assert that we are inside WordPress plugins directory.
 } else {
     require __DIR__ . '/../../../../tests/phpunit/includes/bootstrap.php';
